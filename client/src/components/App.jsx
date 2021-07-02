4b161f327fe986276d646387901ce0e6f356dfbc
Saurabh Kumar <saurabhkumar3034@gmail.com>
2021-07-03 02:42:53 +0530

    Cards showing problem fixed.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,12 +2,28 @@ import React from 'react';
 import Header from './Header'
 import Card from './Card'
 import Footer from './Footer'
+import data from '../data';
 
 function App() {
   return (
     <div className="grid-container">
         <Header />
-        <Card />
+        <main>
+            <div className="row center">
+                {
+                    data.products.map((product => {
+                        return(
+                            <Card 
+                                key = {product._id}
+                                name={product.name} 
+                                image={product.image}
+                                price={product.price}
+                            />
+                        );
+                    }))
+                }
+            </div>
+        </main>
         <Footer />
     </div>
   );
